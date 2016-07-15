b028dfb0be4177d34a962bb81c1b574763ca1721
NickGarcia <nick.agarcia103@gmail.com>
2016-07-15 14:21:32 -0500

    updates for overlays

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
     <link href="../../css/simple-sidebar.css" rel="stylesheet">
+    <link href="../../css/custom.css" rel="stylesheet">
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
--- home.php
+++ home.php
@@ -9,10 +9,12 @@
                         </div>
                         <div class="col-lg-5">
                         	<div class="top homepage-back">
-                        		<div class="photo-overlay"><img src="http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-26.jpg"><h3>Portfolio</h3></div>
+                        		<img src="http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-26.jpg">
+                        		<div class="photo-overlay"><h3>Portfolio</h3></div>
                         	</div>
                         	<div class="bottom homepage-back">
-                        		<div class="photo-overlay"><img src="http://room-decorating-ideas.com/wp-content/uploads/2015/07/lamp-geometric-design-sketch-manufacturer-Indoor-lighting.jpg"><h3>Blog</h3></div>
+                        		<img src="http://room-decorating-ideas.com/wp-content/uploads/2015/07/lamp-geometric-design-sketch-manufacturer-Indoor-lighting.jpg">
+                        		<div class="photo-overlay"><h3>Blog</h3></div>
                         	</div>
                         </div>
                       	</div>
