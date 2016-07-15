d6f6fbc0060f2ce18f59eb86d2f49a7f1651c896
NickGarcia <nick.agarcia103@gmail.com>
2016-07-15 16:16:07 -0500

    add about section to homepage

--- home.php
+++ home.php
@@ -1,10 +1,9 @@
             <div class="container-fluid mainContent">
                 <div class="row">
                     <div class="col-lg-12">
-                        <div class="col-lg-12">
                         <div class="col-lg-7 homepage-back">
-                        	<img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg">
-                        	<div class="photo-overlay"><h3>About Me</h3></div>
+                        	<a href="../../#about"><img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg">
+                        	<div class="photo-overlay"><h3>About Me</h3></div></a>
                         </div>
                         <div class="col-lg-5">
                         	<div class="top homepage-back">
@@ -17,6 +16,25 @@
                         	</div>
                         </div>
                       	</div>
+                </div>
+            </div>
+            <div class="container-fluid mainContent" id="about">
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div class="snippet">
+                        	<img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg">
+
+<p>Lorem ipsum dolor sit amet, mea consulatu referrentur eu, mei brute eruditi in. Ne vel rebum alterum feugiat, et corpora honestatis est. Id tale brute omnes nec. Sed ne dicit latine insolens, in laudem persius honestatis vix, populo graecis no mei. Id iudico convenire nam. Erant ocurreret cu his.</p>
+
+<p>Te copiosae adipisci maluisset has, cum porro harum nonumy in, ut laoreet nominati tractatos nec. Cum no iriure labores delectus. No sit postea vulputate. Ius vero feugiat fuisset ei, cu mei essent everti postulant.</p>
+
+<p>Usu omnium deseruisse referrentur ex, duo tantas commune ad. Discere denique eu eam. Eos ne brute feugait, qui autem nonumes dolores ei, eum consul libris diceret ad. Putant menandri complectitur vim et, et eos sensibus hendrerit interpretaris. Te senserit imperdiet quaerendum pro, ad mei labitur habemus, ut nam exerci laoreet verterem. Sed ut verterem platonem reprimique, debitis apeirian intellegat te mei. Nulla mucius vel id, vim posse theophrastus te, quo cu ridens tritani albucius.</p>
+
+<p>At nam causae feugait minimum, eu hinc lobortis scripserit mel. Eu cum vocibus qualisque, doctus admodum inciderint ne pri. Eum id adversarium suscipiantur, pro ne vitae quidam, ad mel viderer complectitur. Ea eum adhuc movet dictas, nonumes inimicus eum ex. Homero accusamus tincidunt ei nec, senserit pertinacia ne mel.</p>
+
+<p>Vix te utamur luptatum, usu te patrioque moderatius. In summo tritani mnesarchum his. An prodesset consetetur definitiones mel, in choro numquam aliquando vis. Mel elitr tincidunt adipiscing ut.</p>
+</div>
                    	</div>
                 </div>
             </div>
