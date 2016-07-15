2590aca1f9e303772178b9694c8b2d0a1894900e
NickGarcia <nick.agarcia103@gmail.com>
2016-07-15 10:01:24 -0500

    updating files structure

--- home.php
+++ home.php
@@ -29,9 +29,12 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1>Simple Sidebar</h1>
-                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                        <h1>Nick Garcia</h1>
+                        <div class="col-lg-12">
+                        <div class="col-lg-7"><img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg"><h3>About Me</h3></div>
+                        <div class="col-lg-5"><div class="top"><img src="http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-26.jpg">Portfolio</div><div div class="bottom"><img src="http://room-decorating-ideas.com/wp-content/uploads/2015/07/lamp-geometric-design-sketch-manufacturer-Indoor-lighting.jpg">Blog</div></div>
+                        </div>
+
                         <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
                     </div>
                 </div>
