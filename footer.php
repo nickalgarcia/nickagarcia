1d28e4f6c686527976273757eba62714a91852c4
NickGarcia <nick.agarcia103@gmail.com>
2016-07-15 16:34:39 -0500

    adding smooth scrolling

--- footer.php
+++ footer.php
@@ -47,6 +47,24 @@ $(document).ready(function () {
         $('#wrapper').toggleClass('toggled');
   });  
 });
+
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+
     </script>
   </body>
 </html>
