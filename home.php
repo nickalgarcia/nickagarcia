46da5dab2ad08f42420cfa9beab4adc5a2e1edd1
NickGarcia <nick.agarcia103@gmail.com>
2016-07-15 14:12:20 -0500

    adding overlay to images

--- home.php
+++ home.php
@@ -1,15 +1,22 @@
-
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-lg-12">
                         <h1>Nick Garcia</h1>
                         <div class="col-lg-12">
-                        <div class="col-lg-7"><img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg"><h3>About Me</h3></div>
-                        <div class="col-lg-5"><div class="top"><img src="http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-26.jpg">Portfolio</div><div div class="bottom"><img src="http://room-decorating-ideas.com/wp-content/uploads/2015/07/lamp-geometric-design-sketch-manufacturer-Indoor-lighting.jpg">Blog</div></div>
+                        <div class="col-lg-7 homepage-back">
+                        	<img src="http://blog.iso50.com/wp-content/uploads/2012/07/IMG_0084.jpg">
+                        	<div class="photo-overlay"><h3>About Me</h3></div>
                         </div>
-
-                        
-                    </div>
+                        <div class="col-lg-5">
+                        	<div class="top homepage-back">
+                        		<div class="photo-overlay"><img src="http://bashooka.com/wp-content/uploads/2014/06/geometry-design-inspiration-26.jpg"><h3>Portfolio</h3></div>
+                        	</div>
+                        	<div class="bottom homepage-back">
+                        		<div class="photo-overlay"><img src="http://room-decorating-ideas.com/wp-content/uploads/2015/07/lamp-geometric-design-sketch-manufacturer-Indoor-lighting.jpg"><h3>Blog</h3></div>
+                        	</div>
+                        </div>
+                      	</div>
+                   	</div>
                 </div>
             </div>
 
