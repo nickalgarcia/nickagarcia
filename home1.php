c89eb9d96ddd29185bbba8f4cb2260aeadc1efd3
Blackbaud-NickGarcia <nick.garcia@blackbaud.com>
2016-07-14 22:54:25 -0500

    redesign of site

---
+++ home1.php
@@ -0,0 +1,106 @@
+  <section id="home">
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          
+
+          <div class="inner cover">
+            <h1 class="cover-heading"></h1>
+            <p class="lead">Web Design & Development</p>
+
+            <div class="row">
+            <div class="col-md-7"><img src="http://www.telegraph.co.uk/content/dam/Travel/leadAssets/34/97/comedy-hamster_3497562a-large.jpg"></div>
+            <div class="col-md-5"><img src="https://timedotcom.files.wordpress.com/2015/12/amazing-animal-photos-2015-003.jpg?quality=75&strip=color&w=812">
+            <img src="https://static.pexels.com/photos/35435/pexels-photo.jpg"></div>
+            </div>
+
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    </section>
+    <section id="about">
+        <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+                <div class="inner cover">
+                    <h1 class="cover-heading">Headline</h1>
+                    <p class="lead">Lorem ipsum dolor sit amet, solet facilisi ne est, vis quando inermis te, at quo amet summo efficiendi. Per ei altera facilis, pro modo dicta et, ei habeo ignota perpetua nam. Pri et tempor libris regione. Sea blandit imperdiet liberavisse no, no his amet graece suscipit. Eu quas magna signiferumque pro, eos aliquip recteque id. Ad has errem commodo, esse efficiantur pro at, duo diam nibh putant ex. Cu modus dolore audire nec, option accusamus constituam eum ex. Prodesset temporibus per ea, eam purto vero velit ad. Dolorum inciderint id pri, audiam aperiri volumus ne mei. Esse saperet eloquentiam nam id. Ad dicunt adipisci splendide quo. Ad dolor expetenda mea. Suas bonorum definitiones vim ea. Ad ocurreret reformidans mediocritatem nam. Ne sint patrioque sea. Id mei delectus sensibus instructior. Movet explicari theophrastus qui an. Ei est soleat legere consectetuer, cum ex offendit salutatus quaerendum, eu vix regione oblique liberavisse. Sea ceteros denique ne, te mei quem conclusionemque. Novum sonet denique cu eum. Cu vitae veniam complectitur vix, putant deleniti legendos mei ad. An sed munere doctus, quo cu altera saperet. In pro detracto intellegat philosophia, utinam audiam partiendo in vim. Nick</p>
+                </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    </section>
+    <section id="work">
+      <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+          <div class="container">
+          <div class="inner cover">
+            <div class="row">
+              <div class="col-md-3 col-sm-6"><a href="../../portfolio/hey-lolly/"><img class="img-responsive" src="../../images/heylolly.png" alt="Hey Lolly Music"/></a><h3>Hey Lolly</h3></div>
+              <div class="col-md-3 col-sm-6"><img class="img-responsive" src="../../images/honk.png" alt="HONK!TX" /><h3>HONK!TX</h3></div>
+              <div class="col-md-3 col-sm-6"><img class="img-responsive" src="../../images/bmg.png" alt="Bell Media Group" /><h3>Bell Media Group</h3></div>
+              <div class="col-md-3 col-sm-6"><img class="img-responsive" src="../../images/landr.png" alt="L & R Signs" /><h3>L & R Signs</h3></div>
+            </div>
+            <div class="row">
+              <div class="col-md-3 col-sm-6"><img class="img-responsive" src="../../images/ffa.png" alt="Fund for Animals" /><h3>Fund for Animals</h3></div>
+            </div>
+          </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="inner cover">
+            <form method="post" action="../../contact-form/contactengine.php">
+              <div class="row">
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" type="text" name="Name" id="Name" placeholder="Name" />
+                  </div>
+                  <div class="form-group"> 
+                    <input class="form-control" type="email" name="Email" id="Email" placeholder="Email" />
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                   <textarea class="form-control" name="Message" rows="3" id="Message" placeholder="Message" ></textarea>
+                  </div>
+                </div>
+              </div>
+              <input type="submit" name="submit" value="Submit" class="btn btn-info" />
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </section>
