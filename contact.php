590247cc891d5451b8c41b7472dce452f3b98a59
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 17:23:44 -0500

    add contact page

---
+++ contact.php
@@ -0,0 +1,27 @@
+<div class="container-fluid mainContent" id="home">
+	<div class="row">
+		<div class="col-lg-12">
+		<h2>Get in touch</h2>
+		<p>Send us a message using the form below, or email us</p>
+		<p class="email"><a href="mailto:nick.agarcia103@gmail.com">nick.agarcia103@gmail.com</a></p>
+			<form method="post" action="../../contact-form/contactengine.php">
+      	<div class="row">
+        	<div class="col-md-6">
+                  <div class="form-group">
+                    <input class="form-control" type="text" name="Name" id="Name" placeholder="Name" />
+                  </div>
+                  <div class="form-group"> 
+                    <input class="form-control" type="email" name="Email" id="Email" placeholder="Email" />
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-group">
+                   <textarea class="form-control" name="Message" rows="3" id="Message" placeholder="Message" ></textarea>
+                  </div>
+                </div>
+              </div>
+              <input type="submit" name="submit" value="Submit" class="btn btn-info" />
+    	</form>
+		</div>
+	</div>
+</div>
