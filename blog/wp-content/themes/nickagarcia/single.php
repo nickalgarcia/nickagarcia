cea8e6ca17d46e5466b41186646623568fc64098
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 15:45:06 -0500

    update to single page php file

--- blog/wp-content/themes/nickagarcia/single.php
+++ blog/wp-content/themes/nickagarcia/single.php
@@ -13,35 +13,40 @@ get_header(); ?>
 	<div class="row">
 		<div class="col-lg-12">
 
-		<?php
+		<?php the_date(); echo "<br />"; ?>
+<?php the_title(); ?>    
+<?php the_excerpt(); ?> 
+
+		<!--<?php
 		// Start the loop.
-		while ( have_posts() ) : the_post();
+		//while ( have_posts() ) : the_post();
 
 			/*
 			 * Include the post format-specific template for the content. If you want to
 			 * use this in a child theme, then include a file called called content-___.php
 			 * (where ___ is the post format) and that will be used instead.
 			 */
-			get_template_part( 'content', get_post_format() );
+		//	get_template_part( 'content', get_post_format() );
+
 
 			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+		//	if ( comments_open() || get_comments_number() ) :
+		//		comments_template();
+		//	endif;
 
 			// Previous/next post navigation.
-			the_post_navigation( array(
+			/*the_post_navigation( array(
 				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
 					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
 				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
 					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
 					'<span class="post-title">%title</span>',
-			) );
+			) );*/
 
 		// End the loop.
-		endwhile;
-		?>
+		//endwhile;
+		?> -->
 
 		</div>
 	</div>
