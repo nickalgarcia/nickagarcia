aa7032b67eabfa104b2c43a931a06ec9c8ae7efe
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 17:37:40 -0500

    updating portfolio images

--- portfolio.php
+++ portfolio.php
@@ -2,32 +2,32 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<div class="row">
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/heylolly.png">
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="heylolly"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">Hey Lolly Music</div>
   				<div class="portfolio-url"><a href="http://heylollymusic.com/">heylollymusic.com</a></div>
   			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/honk.png">
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="honk"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">HONKTX!</div>
   				<div class="portfolio-url"><a href="http://honktx.org/">honktx.org</a></div>
   			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/landr.png">
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="landr"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">L & R Signs</div>
   				<div class="portfolio-url"><a href="http://landrsigns.com/">landrsigns.com</a></div>
   			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/bmg.png">	
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="bmg"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">Bell Media Group</div>
   				<div class="portfolio-url"><a href="http://bellmediagroup.net/">bellmediagroup.net</a></div>
   			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/rachel.png">
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="rachel"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">Rachel Barkley Music</div>
   				<div class="portfolio-url"><a href="http://rachelbarkley.com/">rachelbarkley.com</a></div>
   			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/ffa.png">
+  			<div class="col-lg-3 col-md-3 col-sm-4"><div class="ffa"></div>
   				<hr class="portfolio-hr">
   				<div class="portfolio-title">Fund for Animals</div>
   				<div class="portfolio-url"><a href="http://fundforanimals.org/">fundforanimals.org</a></div>
