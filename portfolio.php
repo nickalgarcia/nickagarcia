9806a0b5e5a81ce7539b79fe7214af19da8f24fb
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 16:52:03 -0500

    updates to portfolio page

--- portfolio.php
+++ portfolio.php
@@ -1,10 +1,25 @@
 <div class="container-fluid mainContent" id="home">
 	<div class="row">
 		<div class="col-lg-12">
-			<div class="grid">
-  			<div class="grid-item"><img src="../../images/heylolly.png"></div>
-  			<div class="grid-item grid-item--width2"><img src="../../images/honk.png"></div>
-  			<div class="grid-item"><img src="../../images/landr.png"></div>
+			<div class="row">
+  			<div class="col-lg-3"><img src="../../images/heylolly.png">
+  				<div class="portfolio-title">Hey Lolly Music</div>
+  			</div>
+  			<div class="col-lg-3"><img src="../../images/honk.png">
+  				<div class="portfolio-title">HONKTX!</div>
+  			</div>
+  			<div class="col-lg-3"><img src="../../images/landr.png">
+  				<div class="portfolio-title">L & R Signs</div>
+  			</div>
+  			<div class="col-lg-3"><img src="../../images/bmg.png">
+  				<div class="portfolio-title">Bell Media Group</div>
+  			</div>
+  			<div class="col-lg-3"><img src="../../images/rachel.png">
+  				<div class="portfolio-title">Rachel Barkley Music</div>
+  			</div>
+  			<div class="col-lg-3"><img src="../../images/ffa.png">
+  				<div class="portfolio-title">Fund for Animals</div>
+  			</div>
 			</div>
 		</div>
 	</div>
