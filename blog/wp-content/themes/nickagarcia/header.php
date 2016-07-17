ebc3417ced4fe158a58b544bd4690c376a86310a
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 18:03:46 -0500

    add favicon

--- blog/wp-content/themes/nickagarcia/header.php
+++ blog/wp-content/themes/nickagarcia/header.php
@@ -16,6 +16,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="icon" href="http://nickagarcia.com/images/favicon.ico">
     <title>Nick Garcia</title>
 
     <!-- Bootstrap core CSS -->
