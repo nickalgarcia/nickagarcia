0108c0df2c6c0a766a9d52ec20b28c2a7f500dfd
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 16:20:52 -0500

    remove files

--- portfolio/hey-lolly/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-error_reporting(E_ALL & ~E_NOTICE);
-//require_once('header.php');
-require_once('http://nickagarcia.com/portfolio/header.php');
-require_once(basename(($_GET['page']!='' ? $_GET['page'] : 'home') . '.php'));
-require_once('http://nickagarcia.com/footer.php');
-?>
