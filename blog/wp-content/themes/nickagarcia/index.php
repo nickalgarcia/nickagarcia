4dbedbd252ad000dab5df4e954d43b2d0c95a44e
NickGarcia <nick.agarcia103@gmail.com>
2016-07-17 15:23:13 -0500

    update to blog index

--- blog/wp-content/themes/nickagarcia/index.php
+++ blog/wp-content/themes/nickagarcia/index.php
@@ -37,16 +37,16 @@ get_header(); ?>
 				 * If you want to override this in a child theme, then include a file
 				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 				 */
-				get_template_part( 'content', get_post_format() );
+				get_template_part( 'content', /*get_post_format()*/the_content() );
 
 			// End the loop.
 			endwhile;
 
 			// Previous/next page navigation.
 			the_posts_pagination( array(
-				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-				'next_text'          => __( 'Next page', 'twentyfifteen' ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+				'prev_text'          => __( 'Previous page', 'nickagarcia' ),
+				'next_text'          => __( 'Next page', 'nickagarcia' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'nickagarcia' ) . ' </span>',
 			) );
 
 		// If no content, include the "No posts found" template.
