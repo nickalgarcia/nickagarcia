15aa3e6a64cf254e990d63cc77d1d4bbfe6e7234
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 21:54:44 -0500

    update portfolio links

--- portfolio.php
+++ portfolio.php
@@ -2,36 +2,53 @@
 	<div class="row">
 		<div class="col-lg-12">
 			<div class="row">
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/heylolly.png">
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">Hey Lolly Music</div>
-  				<div class="portfolio-url"><a href="http://heylollymusic.com/">heylollymusic.com</a></div>
-  			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/honk.png">
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">HONKTX!</div>
-  				<div class="portfolio-url"><a href="http://honktx.org/">honktx.org</a></div>
-  			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/landr.png">
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">L & R Signs</div>
-  				<div class="portfolio-url"><a href="http://landrsigns.com/">landrsigns.com</a></div>
-  			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/bmg.png">	
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">Bell Media Group</div>
-  				<div class="portfolio-url"><a href="http://bellmediagroup.net/">bellmediagroup.net</a></div>
-  			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/rachel.png">
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">Rachel Barkley Music</div>
-  				<div class="portfolio-url"><a href="http://rachelbarkley.com/">rachelbarkley.com</a></div>
-  			</div>
-  			<div class="col-lg-3 col-md-3 col-sm-4"><img src="../../images/ffa.png">
-  				<hr class="portfolio-hr">
-  				<div class="portfolio-title">Fund for Animals</div>
-  				<div class="portfolio-url"><a href="http://fundforanimals.org/">fundforanimals.org</a></div>
-  			</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=heylolly"><img src="../../images/heylolly.png">
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">Hey Lolly Music</div>
+				</a>
+					<div class="portfolio-url"><a href="http://heylollymusic.com/">heylollymusic.com</a></div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=honktx">
+					<img src="../../images/honk.png">
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">HONKTX!</div>
+				</a>
+					<div class="portfolio-url"><a href="http://honktx.org/">honktx.org</a></div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=landr">
+					<img src="../../images/landr.png">
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">L & R Signs</div>
+				</a>
+					<div class="portfolio-url"><a href="http://landrsigns.com/">landrsigns.com</a></div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=bmg">
+					<img src="../../images/bmg.png">	
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">Bell Media Group</div>
+				</a>
+					<div class="portfolio-url"><a href="http://bellmediagroup.net/">bellmediagroup.net</a></div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=rachel">
+					<img src="../../images/rachel.png">
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">Rachel Barkley Music</div>
+				</a>
+					<div class="portfolio-url"><a href="http://rachelbarkley.com/">rachelbarkley.com</a></div>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-4">
+				<a href="?page=ffa">
+					<img src="../../images/ffa.png">
+					<hr class="portfolio-hr">
+					<div class="portfolio-title">Fund for Animals</div>
+				</a>
+					<div class="portfolio-url"><a href="http://fundforanimals.org/">fundforanimals.org</a></div>
+				</div>
 			</div>
 		</div>
 	</div>
