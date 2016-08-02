2ff0d1bad20525cb102bc4da091d934f01266663
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 21:12:33 -0500

    moved heylolly portfolio page

--- portfolio/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-error_reporting(E_ALL & ~E_NOTICE);
-define("SITE_ROOT", "/var/www/nickagarcia");
-
-require_once(SITE_ROOT. 'header.php');
-require_once(basename(($_GET['page']!='' ? $_GET['page'] : 'home') . '.php'));
-require_once(SITE_ROOT. 'footer.php');
-?>
