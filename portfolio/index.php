62bdfcee52b2f64d21b161cecfdd6662e3476287
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 20:48:37 -0500

    adding site root

--- portfolio/index.php
+++ portfolio/index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-require_once('/header.php');
+define("SITE_ROOT", "/var/www/nickagarcia");
+
+require_once(SITE_ROOT. '/header.php');
 require_once(basename(($_GET['page']!='' ? $_GET['page'] : 'home') . '.php'));
-require_once('/footer.php');
+require_once(SITE_ROOT. '/footer.php');
 ?>
