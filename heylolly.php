2c3ae81041f7d0057a85f03d8b851e597c9505f9
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 21:29:36 -0500

    adding content, next is styling

--- heylolly.php
+++ heylolly.php
@@ -3,6 +3,9 @@
 		<div class="col-lg-12">
 		<img src="/images/portfolio/hey-lolly1.png">
 		<h2>Hey Lolly Music</h2>
+		<p>Laura Freeman created Hey Lolly Music to be an interactive and educational experience for children through music and puppetry. The previous website was very dated with a poor user experience. With design provided by Kara Fuhlbrugge from <a href="http://cmykara.com">cmykara.com</a>, we were able to create a site that is both visually appealing as well as easy to navigate.</p>
+		<p>A single page website seemed most appropriate for Hey Lolly Music.</p>
+		<img src="/images/portfolio/hey-lolly2.png">
 		</div>
 	</div>
 </div>
