c2a7bc5c3b6240e7381888a126a876611f0dcc66
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 20:17:44 -0500

    adding a signature page for my website

--- header.php
+++ header.php
@@ -18,6 +18,8 @@
 
     <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Homemade+Apple" rel="stylesheet">
+
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <link href="../../css/ie10-viewport-bug-workaround.css" rel="stylesheet">
---
+++ signature.php
@@ -0,0 +1,7 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<div class="signature">Nick Garcia</div>
+		</div>
+	</div>
+</div>
