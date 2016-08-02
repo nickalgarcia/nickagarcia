a9eedc7383e723d00ca9f465ba669800eaa680cc
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 21:19:10 -0500

    optimized images

--- heylolly.php
+++ heylolly.php
@@ -1,7 +1,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-lg-12">
-		<img src="/images/heylolly.png">
+		<img src="/images/portfolio/hey-lolly1.png">
 		<h2>Hey Lolly Music</h2>
 		</div>
 	</div>
