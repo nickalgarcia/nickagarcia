34b194de8c777db92e464e3fe2fac347a892b7b1
NickGarcia <nick.agarcia103@gmail.com>
2016-08-01 21:50:52 -0500

    new portfolio pages with placeholder content

---
+++ bmg.php
@@ -0,0 +1,10 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<img src="/images/portfolio/bmg.png">
+		<h2>Bell Media Group</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime illo quis iusto obcaecati ipsum optio enim cupiditate ex temporibus possimus nobis, autem quaerat aut eaque vero tempore pariatur deserunt.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nostrum cupiditate cum doloribus tempora nisi, nobis vel quod, consectetur, maxime tenetur. Expedita nisi cupiditate, quod fugit itaque voluptates optio veritatis?</p>
+		</div>
+	</div>
+</div>
---
+++ honktx.php
@@ -0,0 +1,11 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<img src="/images/portfolio/honk1.png">
+		<h2>HONK!TX</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit maxime illo quis iusto obcaecati ipsum optio enim cupiditate ex temporibus possimus nobis, autem quaerat aut eaque vero tempore pariatur deserunt.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit nostrum cupiditate cum doloribus tempora nisi, nobis vel quod, consectetur, maxime tenetur. Expedita nisi cupiditate, quod fugit itaque voluptates optio veritatis?</p>
+		<img src="/images/portfolio/honk2.png">
+		</div>
+	</div>
+</div>
