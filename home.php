5c20987a0361b28d0e711fd9e58dd519d17dcac6
NickGarcia <nick.agarcia103@gmail.com>
2016-08-03 16:17:27 -0500

    add resume link to homepage

--- home.php
+++ home.php
@@ -29,7 +29,7 @@
 
 <p>Hi! My name is Nick Garcia and I'm a Web Developer based out of Austin, TX. I primarily do front-end development for Non-Profits, Churches, Small Businesses, and musicians. I use HTML, CSS, and Javascript as well as PHP when developing sites using Wordpress. I have 7 years experience with front-end development. My experience includes working for within an organization as the Web Developer as well as serving multiple clients.</p>
 <p>For more of my work experience and qualifications, click below to download my resume.</p>
-<div class="resume"><i class="fa fa-file-text-o"></i><span>Download Resume</span></div>
+<div class="resume"><a href="?page=resume"><i class="fa fa-file-text-o"></i><span>View Resume</span></a></div>
 </div>
                     </div>
                 </div>
