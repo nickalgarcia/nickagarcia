8744887c3549c4127e3ef5dc5fe0619a04e14169
NickGarcia <nick.agarcia103@gmail.com>
2016-08-03 15:30:47 -0500

    add a resume page

---
+++ resume.php
@@ -0,0 +1,45 @@
+<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+		<div class="col-lg-8">
+			<h2>Work Experience</h2>
+			<div class="work-title">Web Developer</div>
+			<div class="work-employer">Blackbaud</div>
+			<div class="work-dates">December 2014 - Present</div>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut saepe libero reprehenderit quia facere quos natus vitae voluptatibus quisquam aperiam laboriosam at, eum laudantium, debitis autem animi qui veniam iure?</li>
+			</ul>
+			<div class="work-title">Web Content Manager</div>
+			<div class="work-employer">Special Olympics Texas</div>
+			<div class="work-dates">April 2013 - December 2014</div>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut saepe libero reprehenderit quia facere quos natus vitae voluptatibus quisquam aperiam laboriosam at, eum laudantium, debitis autem animi qui veniam iure?</li>
+			</ul>
+			<div class="work-title">Interactive Marketing Specialist</div>
+			<div class="work-employer">SeaWorld San Antonio</div>
+			<div class="work-dates">March 2012 - April 2013</div>
+			<ul>
+				<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut saepe libero reprehenderit quia facere quos natus vitae voluptatibus quisquam aperiam laboriosam at, eum laudantium, debitis autem animi qui veniam iure?</li>
+			</ul>
+		</div>
+		<div class="col-lg-4">
+			<h2>Education</h2>
+			<div class="education">Master of Business Administration - Marketing <span>Our Lady of the Lake University</span></div>
+			<div class="education">Bachelor of Music - Music Marketing <span>University of Texas at San Antonio</span></div>
+			<hr>
+			<div class="skills">
+				<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>jQuery</li>
+				<li>Javascript</li>
+				<li>Wordpress Development</li>
+				<li>Github</li>
+				<li>Less</li>
+				<li>Grunt</li>
+				</ul>
+			</div>
+		</div>
+		</div>
+	</div>
+</div>
