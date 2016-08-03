616439415e76a60cef88d728a33e8cbcb3f0c11a
NickGarcia <nick.agarcia103@gmail.com>
2016-08-03 14:20:54 -0500

    fix class

--- honktx.php
+++ honktx.php
@@ -3,11 +3,11 @@
 		<div class="col-lg-12 portfolio">
 		<img src="/images/portfolio/honk1.png">
 		<h2 class="title">HONK!TX</h2>
-		<div class="md-col-6">
+		<div class="col-lg-6">
 		<h4>About Overview</h4>
 			<p>HONK!TX is a nonprofit, community-driven festival that brings street and brass bands from around the country to perform for free in the public spaces of Austin, Texas. Mobile and unamplified, HONK! bands transform everyday locations into spontaneous stages, dissolving the line between crowd and performers and inviting everyone to celebrate the joys of music and community.</p>
 		</div>
-		<div class="md-col-6">
+		<div class="col-lg-6">
 		<h4>Details</h4>
 			<p>This is another project that I worked on with Kara from <a href="http://cmykara.com" target="_blank">cmykara.com</a>. Kara delivered designs for the site and I made recommendations on how to modify elements to work seamlessly in Wordpress for the client to maintain. This is one of the projects that I am most proud of having worked on. The design is bold and perfectly displays the atmosphere of a HONK!TX event.</p>
 		</div>
