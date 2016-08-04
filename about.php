83507078a967549282df1dd4e77b97b456e7fd8b
NickGarcia <nick.agarcia103@gmail.com>
2016-08-04 14:21:21 -0500

    clean up homepage and about

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 	<div class="snippet">
 		<img src="/images/nick.png">
 		<p>Hi! My name is Nick Garcia and I'm a Web Developer based out of Austin, TX. I primarily do front-end development for Non-Profits, Churches, Small Businesses, and musicians. I use HTML, CSS, and Javascript as well as PHP when developing sites using Wordpress. I have 7 years experience with front-end development. My experience includes working for within an organization as the Web Developer as well as serving multiple clients.</p>
-		<p>For more of my work experience and qualifications, click below to download my resume.</p>
+		<p>For more of my work experience and qualifications, click below to view my resume.</p>
 		<div class="resume"><a href="?page=resume"><i class="fa fa-file-text-o"></i><span>View Resume</span></a></div>
 	</div>
 </div>
