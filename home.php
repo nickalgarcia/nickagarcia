204fb6706c001bd3af5471d2c59c48fb20392057
NickGarcia <nick.agarcia103@gmail.com>
2016-08-03 21:11:25 -0500

    add row to home page

--- home.php
+++ home.php
@@ -1,21 +1,23 @@
-                        <div class="col-lg-7">
-                          <div class="homepage-back">
-                            <a href="#about"><img src="/images/about.png">
-                            <div class="photo-overlay"><h3>About Me</h3></div></a>
-                          </div>
-                        </div>
-                        <div class="col-lg-5">
-                          <div class="top homepage-back">
-                            <a href="?page=portfolio"><img src="http://blackrockdigital.github.io/startbootstrap-creative/img/header.jpg">
-                            <div class="photo-overlay"><h3>Portfolio</h3></div></a>
-                          </div>
-                          <div class="bottom homepage-back">
-                            <a href="../../blog/""><img src="http://i.huffpost.com/gen/987952/images/o-KEEPING-A-JOURNAL-facebook.jpg">
-                            <div class="photo-overlay"><h3>Blog</h3></div></a>
-                          </div>
-                        </div>
-                        
-            <?php
+<div class="row">
+	<div class="col-lg-7">
+		<div class="homepage-back">
+														<a href="#about"><img src="/images/about.png">
+														<div class="photo-overlay"><h3>About Me</h3></div></a>
+													</div>
+												</div>
+												<div class="col-lg-5">
+													<div class="top homepage-back">
+														<a href="?page=portfolio"><img src="http://blackrockdigital.github.io/startbootstrap-creative/img/header.jpg">
+														<div class="photo-overlay"><h3>Portfolio</h3></div></a>
+													</div>
+													<div class="bottom homepage-back">
+														<a href="../../blog/""><img src="http://i.huffpost.com/gen/987952/images/o-KEEPING-A-JOURNAL-facebook.jpg">
+														<div class="photo-overlay"><h3>Blog</h3></div></a>
+													</div>
+												</div>
+												</div>
+												
+						<?php
 include 'about.php';
 ?>
 
