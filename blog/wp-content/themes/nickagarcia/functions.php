f193aa83a45f2bfd8178e6ec1e0a6a7399a5ec50
NickGarcia <nick.agarcia103@gmail.com>
2016-08-10 15:50:03 -0500

    trying something new

--- blog/wp-content/themes/nickagarcia/functions.php
+++ blog/wp-content/themes/nickagarcia/functions.php
@@ -102,6 +102,12 @@ function nickagarcia_setup() {
 	add_theme_support( 'post-formats', array(
 		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
 	) );
+	
+	add_filter( 'embed_oembed_html', 'tdd_oembed_filter', 10, 4 ) ; 
+	function tdd_oembed_filter($html, $url, $attr, $post_ID) {
+    $return = '<div class="embed-responsive embed-responsive-16by9">'.$html.'</div>';
+    return $return;
+	}
 
 	/*
 	 * Enable support for custom logo.
