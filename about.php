145924c5b96c84dda96bccf98333f179903a64a6
NickGarcia <nick.agarcia103@gmail.com>
2016-08-25 22:12:24 -0500

    adding custom css and hide mobile header

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <div class="row">
 <div id="about">
+<h2>About Me</h2>
 	<div class="snippet">
 		<img src="/images/nick.png">
 		<p>Hi! My name is Nick Garcia and I'm a Web Developer based out of Austin, TX. I primarily do front-end development for Non-Profits, Churches, Small Businesses, and musicians. I use HTML, CSS, and Javascript as well as PHP when developing sites using Wordpress. I have 7 years experience with front-end development. My experience includes working for within an organization as the Web Developer as well as serving multiple clients.</p>
