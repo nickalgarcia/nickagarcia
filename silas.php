32742e3a327709d1edb6413ae46f903226860a75
NickGarcia <nick.agarcia103@gmail.com>
2017-07-07 14:19:51 -0500

    adding content to silas' page

--- silas.php
+++ silas.php
@@ -1,2 +1,27 @@
 <h1>Silas</h1>
+<section class="hero">
+<div class="hero-image">
+<img src="../images/silas-hero.jpg">
+</div>
+<div class="hero-content">
+<div class="fundraising-progress">
+    <div class="fundraising-progress__meter">
+              <ul class="list-unstyled">
+                <li class="thermometer_truck">
+                  <img src="../images/counter/amount--0.png" alt="">
+                </li>
+                <li class="thermometer__percentage">0%</li>
+              </ul>
+            </div>
+    <div class="fundraising-progress_raised">
+    	<h5 class="fundraising-progress_label">RAISED:</h5>
+    	<p class="fundraising-progress_numbers">$0</p>
+    </div>
+    <div class="fundraising-progress_goal">
+    	<h5 class="fundraising-progress_label">RAISED:</h5>
+    	<p class="fundraising-progress_numbers">$3,000</p>
+    </div>
+</div>
 <a class="btn" href="https://www.gofundme.com/hope-for-silas">Give</a>
+</div>
+</section>
