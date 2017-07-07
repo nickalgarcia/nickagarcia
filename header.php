756edc7a72891f58a900be758858d5560d91ccce
NickGarcia <nick.agarcia103@gmail.com>
2017-07-07 17:44:19 -0500

    trying to fix something

--- header.php
+++ header.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "site-content" div.
+ *
+ * @package WordPress
+ * @subpackage nickagarcia
+ * @since nickagarcia 1.0
+ */
+?>
 <!DOCTYPE html>
 <html lang="en">
 
