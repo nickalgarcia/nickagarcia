9e6ee10b4134fd379e6bec208c2fc82896be50a7
NickGarcia <nick.agarcia103@gmail.com>
2017-07-07 17:46:21 -0500

    updating header for blog

---
+++ blog/footer.php
@@ -0,0 +1,40 @@
+</div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+    </div>
+    <!-- /#wrapper -->
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"><\/script>')</script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/headroom/0.9.3/headroom.min.js"></script>
+
+    <script src="../../js/custom.min.js"></script>
+
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="https://maxcdn.bootstrapcdn.com/js/ie10-viewport-bug-workaround.js"></script>
+
+        <!-- Menu Toggle Script -->
+
+
+<?php
+$silas = "/?page=silas";
+$currentpage = $_SERVER['REQUEST_URI'];
+$page = $_SERVER['REQUEST_URI']; 
+$page = str_replace('/', '', $page); 
+$page = str_replace('.php', '', $page); 
+$page = str_replace('?page=', '', $page); 
+$page = $page ? $page : 'home';
+if ($silas==$currentpage): ?>
+<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-58507fe68b85039f"></script> 
+<?php else: ?>
+<?php endif; ?>
+  </body>
+</html>
