e94b0580ffd6e9d90fa5116c18d9e13a5508fd7e
NickGarcia <nick.agarcia103@gmail.com>
2017-07-08 12:54:35 -0500

    remove test code

--- js/custom.js
+++ js/custom.js
@@ -13,20 +13,4 @@ $(document).ready(function() {
     });
 });
 
-$(function() {
-    //$('.videos div').first().addClass('latest-video').removeClass('all-videos');
-    $('.all-videos').wrapAll("<div class='sidebar' />");
-});
 
-$.ajax({
-    dataType: 'html',
-    url: 'https://www.gofundme.com/hope-for-silas',
-    success: function(response) {
-
-        console.log(response);
-
-        //jQuery( "#page-title" ).load( "STR?fr_id=2333&pg=personal&px=3682468&pgwrap=n .span8 strong.heading" );
-
-
-    }
-});
