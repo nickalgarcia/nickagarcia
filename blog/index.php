154f68d1de7e0e2cde6a7d8fcbb6c64a34601acc
NickGarcia <nick.agarcia103@gmail.com>
2017-07-07 19:42:51 -0500

    trying again

--- blog/index.php
+++ blog/index.php
@@ -6,8 +6,8 @@ require('./wp-blog-header.php');
 
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-require_once dirname(__FILE__) . '../../header.php';
-require_once dirname(__FILE__) . '../../footer.php';
+require_once dirname(__FILE__) . '/header.php';
+require_once dirname(__FILE__) . '/footer.php';
 ?>
 
 <?php
