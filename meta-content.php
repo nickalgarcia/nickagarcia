513ac26a1e2f6fb2b10582a698fafdde71d768b7
NickGarcia <nick.agarcia103@gmail.com>
2017-07-08 19:32:10 -0500

    still trying

--- meta-content.php
+++ meta-content.php
@@ -1,17 +1,15 @@
 
 <?php
 // Define variables for SEO
-$pageTitle = '';
-$pageDescription = '';
 
 
 $fileName = basename(__FILE__);
 switch($fileName){
-  case 'about.php':
+  case '/about.php':
     $pageTitle = 'About';
 	$pageDescription = 'This is a test';
     break;
-  case 'portfolio/home.php':
+  case '/portfolio/home.php':
     $pageTitle = 'Portfolio';
 	$pageDescription = 'This is a test';
     break;
