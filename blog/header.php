06385ff87813d25dc5ffd94a9fa8d27f1ee0f737
NickGarcia <nick.agarcia103@gmail.com>
2017-07-08 20:26:32 -0500

    Blog Header update

--- blog/header.php
+++ blog/header.php
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <link rel="icon" href="../../images/favicon.ico">
-    <title>Nick Garcia</title>
+    <title><?php single_post_title(); ?></title>
 
     <!-- Bootstrap core CSS -->
     <!--<link href="../../css/bootstrap.min.css" rel="stylesheet">-->
