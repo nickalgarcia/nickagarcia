2576be23854658d6b7a716eef10f89c3bd50abd7
NickGarcia <nick.agarcia103@gmail.com>
2017-11-16 10:52:55 -0600

    update to playlist

--- playlist.php
+++ playlist.php
@@ -32,6 +32,7 @@ $playlist = json_decode(file_get_contents($api_url));
 
   </div>
   <div class="video-description"><?php echo $item->snippet->description;  ?></div>
+  <div class="video-tag"><?php echo $item->snippet->tag;  ?></div>
 </div>
 <?php else: ?>
   <?php endif; ?>
