8c96f7785bd8b6f87fb8502a0eaee3fd62552c51
NickGarcia <nick.garcia@blackbaud.com>
2018-08-13 15:08:26 -0500

    adding google verification

--- header.php
+++ header.php
@@ -17,6 +17,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="google-site-verification" content="-4H4tceyY5ENQI9IoR8--9jGZwyfP-u9WHzEsC4pOyg" />
 
         <?php
 $homepage = "/";
